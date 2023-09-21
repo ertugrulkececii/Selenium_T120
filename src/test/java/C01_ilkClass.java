ac1fbae5acf2eee3c62c0830620c9a094f385752
ertugrulkececii <ertgrlkcc@gmail.com>
2023-09-21 21:53:06 +0200

    first commit

---
+++ src/test/java/C01_ilkClass.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C01_ilkClass {
+    public static void main(String[] args) {
+
+
+
+System.setProperty("Webdriver.chrome.driver", "chromedriver.exe");
+
+WebDriver driver = new ChromeDriver();
+
+driver.get("https://www.amazon.com");
+    }
+}
---
+++ src/test/java/C02_SeleniumWebDriver.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C02_SeleniumWebDriver {
+    public static void main(String[] args) {
+
+        // ilk adimi atlayip direk WebDriver objesi olusuralim.
+
+        WebDriver driver =new ChromeDriver();
+        driver.get("https://www.amazon.com");
+        /*
+        Selenium son versiyonlarindan birinde kendi webdriver classini ekledi
+        boylece oncesinde driver.exe dosyasini tanitmasakta istedigmiz browser i calistirir
+
+        Sadece oncelikle bizden driver bekledigi icin birazcik gec calisir.
+
+        Sirketler acik kaynakli driver kullanmayi tercih etmez.
+        Genellikle kendi satin aldiklari bir driver'i kullanmamizi isterler
+         */
+    }
+
+}
